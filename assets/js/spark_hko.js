0bf64dfca346bc6d1d0d398629ebcd350842425a
Cyril Ng Lung Kit <nglkitc@yahoo.co.uk>
2025-11-20 21:59:45 +0800

    added HKO marquee to site

--- assets/js/spark_hko.js
+++ assets/js/spark_hko.js
@@ -1,23 +1,25 @@
-//const sparkBaseUrl = "https://spark.cyrilng.com";
-const sparkBaseUrl = "http://localhost:8080";
+const sparkBaseUrl = "https://spark.cyrilng.com";
+//const sparkBaseUrl = "http://localhost:8080";
 
-function subscribeToHkoWarnings() {
+function subscribeToHkoWarnings(eventHandlerFunction) {
+    if (location.hostname === "localhost" || location.hostname === "127.0.0.1" || location.hostname === "") {
+
+    }
     const evtSource = new EventSource(sparkBaseUrl + "/hko/warnings/stream");
     evtSource.onmessage = (event) => {
         const warnings = JSON.parse(event.data);
+        const responseArray = [];
         warnings.details.forEach(element => {
-            console.log("contents: " + element["contents"]);
-            console.log("subtype: " + element["subtype"]);
-            console.log("warningStatementCode: " + element["warningStatementCode"]);
-            console.log("updateTime: " + element["updateTime"]);
             var description;
             if (Object.hasOwn(element, "subtype")) {
                 description = WARNING_DESCRIPTION_MAP[element["subtype"]];
             } else {
                 description = WARNING_DESCRIPTION_MAP[element["warningStatementCode"]];
             }
-            console.log(description);
+            // console.log(JSON.stringify(element));
+            responseArray.push(description);
         });
+        eventHandlerFunction("HKO: " + responseArray.join(" | "));
     };
     evtSource.onerror = (err) => {
         console.error("EventSource failed:", err);
@@ -25,29 +27,40 @@ function subscribeToHkoWarnings() {
     return evtSource;
 };
 
+function addToMarquee(text) {
+    console.log("addToMarquee: " + text);
+    var marquee = document.getElementById("main_marquee");
+    marquee.innerHTML = ""
+
+    var div = document.createElement("div");
+    div.className = "marquee_text";
+    div.textContent = text || "";
+    marquee.appendChild(div);
+}
+
 const WARNING_DESCRIPTION_MAP = {
-  "WFIREY": "Yellow Fire Danger Warning",
-  "WFIRER": "Red Fire Danger Warning",
-  "WRAINA": "Amber Rainstorm Warning",
-  "WRAINR": "Red Rainstorm Warning",
-  "WRAINB": "Black Rainstorm Warning",
-  "TC1": "Standby Signal No.1",
-  "TC3": "Strong Wind Signal No.3",
-  "WTCPRE8": "Pre-8 Tropical Cyclone Special Announcement",
-  "TC8NE": "No.8 North East Gale or Storm",
-  "TC8SE": "No.8 South East Gale or Storm",
-  "TC8SW": "No.8 South West Gale or Storm",
-  "TC8NW": "No.8 North West Gale or Storm",
-  "TC9": "Increasing Gale or Storm No.9",
-  "TC10": "Hurricane Signal No.10",
-  "WTCSGNL": "Tropical Cyclone Signals Cancelled",
-  "CANCEL": "All Signals Cancelled",
-  "WFROST": "Frost Warning",
-  "WHOT": "Very Hot Weather Warning",
-  "WCOLD": "Cold Weather Warning",
-  "WMSGNL": "Strong Monsoon",
-  "WFNTSA": "Flooding in the Northern New Territories",
-  "WL": "Landslip Warning",
-  "WTMW": "Tsunami Warning",
-  "WTS": "Thunderstorm Warning",
+  "WFIREY"  :   "Yellow Fire Danger Warning",
+  "WFIRER"  :   "Red Fire Danger Warning",
+  "WRAINA"  :   "Amber Rainstorm Warning",
+  "WRAINR"  :   "Red Rainstorm Warning",
+  "WRAINB"  :   "Black Rainstorm Warning",
+  "TC1"     :   "Standby Signal No.1",
+  "TC3"     :   "Strong Wind Signal No.3",
+  "WTCPRE8" :   "Pre-8 Tropical Cyclone Special Announcement",
+  "TC8NE"   :   "No.8 North East Gale or Storm",
+  "TC8SE"   :   "No.8 South East Gale or Storm",
+  "TC8SW"   :   "No.8 South West Gale or Storm",
+  "TC8NW"   :   "No.8 North West Gale or Storm",
+  "TC9"     :   "Increasing Gale or Storm No.9",
+  "TC10"    :   "Hurricane Signal No.10",
+  "WTCSGNL" :   "Tropical Cyclone Signals Cancelled",
+  "CANCEL"  :   "All Signals Cancelled",
+  "WFROST"  :   "Frost Warning",
+  "WHOT"    :   "Very Hot Weather Warning",
+  "WCOLD"   :   "Cold Weather Warning",
+  "WMSGNL"  :   "Strong Monsoon",
+  "WFNTSA"  :   "Flooding in the Northern New Territories",
+  "WL"      :   "Landslip Warning",
+  "WTMW"    :   "Tsunami Warning",
+  "WTS"     :   "Thunderstorm Warning",
 };
